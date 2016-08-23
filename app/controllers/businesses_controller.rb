ddb8ad2603319f173619ed4742770502421e3089
cjvirtucio87 <cjv287@gmail.com>
2016-08-23 06:24:49 -0700

    Added form for business.

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -5,6 +5,10 @@ class BusinessesController < ApplicationController
     @businesses = Business.all
   end
 
+  def new
+    @business = Business.new
+  end
+
   def create
     qparams = { '$q' => 'EASTMAN', '$limit' => '3' }
     data = query_soda(qparams)
@@ -17,5 +21,29 @@ class BusinessesController < ApplicationController
       redirect_to businesses_path
     end
   end
+
+  private
+
+    def business_params
+      params.require(:business).permit(
+        :id,
+        :user_id,
+        :ttxid,
+        :certificate_number,
+        :ownership_name,
+        :dba_name,
+        :full_business_address,
+        :city,
+        :state,
+        :business_zip,
+        :dba_start_date,
+        :dba_end_date,
+        :location_start_date,
+        :location_end_date,
+        :mailing_address_1,
+        :mailing_city_state_zip_code,
+        :location
+        )
+    end
 
 end
