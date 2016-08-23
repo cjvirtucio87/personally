a1d838259c27019e5cf112877c873af3431d50c9
cjvirtucio87 <cjv287@gmail.com>
2016-08-23 07:36:35 -0700

    Confirmed working businesses#create.

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -10,16 +10,52 @@ class BusinessesController < ApplicationController
   end
 
   def create
-    qparams = { '$q' => 'EASTMAN', '$limit' => '3' }
-    data = query_soda(qparams)
-    @business = Business.new(data.first.to_h)
-    if @business.save
-      flash[:success] = "Business saved."
-      redirect_to businesses_path
-    else
-      flash[:notice] = "Couldn't save business."
-      redirect_to businesses_path
-    end
+    # begin
+      # Attempt with hashify_query
+      hash_query = query_soda(hashify_query)
+      if hash_query.empty?
+        # Attempt with stringify_query
+        string_query = query_soda('$q' => stringify_query)
+        if string_query.empty?
+          flash[:notice] = "Couldn't find a business with that information"
+          redirect_to new_business_path
+        else
+          # More than one result
+          if string_query.count > 1
+            @data = string_query
+            flash[:success] = "We've found more than one result."
+            redirect_to new_business_path
+          else
+            @business = Business.new(string_query.first.to_h)
+            if @business.save
+              flash[:success] = "Business saved."
+              redirect_to @business
+            else
+              flash[:notice] = "Business could not be saved."
+              redirect_to new_business_path
+            end
+          end
+        end
+      else
+        if hash_query.count > 1
+          @data = hash_query
+          flash[:success] = "We've found more than one result."
+          redirect_to new_business_path
+        else
+          @business = Business.new(hash_query.first.to_h)
+          if @business.save
+            flash[:success] = "Business saved."
+            redirect_to @business
+          else
+            flash[:notice] = "Business could not be saved."
+            redirect_to new_business_path
+          end
+        end
+      end
+    # rescue
+    #   flash[:notice] = "Invalid information."
+    #   redirect_to new_business_path
+    # end
   end
 
   private
@@ -44,6 +80,20 @@ class BusinessesController < ApplicationController
         :mailing_city_state_zip_code,
         :location
         )
+    end
+
+    def hashify_query
+      hash = business_params.to_h
+      hash_upcase = {}
+      hash.each { |k,v| hash_upcase[k] = v.upcase if v != ''}
+      hash_upcase
+    end
+
+    def stringify_query
+      hash_upcase = hashify_query
+      string = ""
+      hash_upcase.each { |k,v| string += "#{v} " if v != '' }
+      string[0..-2]
     end
 
 end
