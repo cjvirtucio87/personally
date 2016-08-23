529e3a2387c896b7a9e6a14173f6e445b3423d81
cjvirtucio87 <cjv287@gmail.com>
2016-08-23 09:26:52 -0700

    Added auth methods to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,28 @@
 class User < ApplicationRecord
+
+  ### Associations ###
   has_one :business, dependent: :nullify
   has_many :bookmarks, dependent: :destroy
   has_many :bookmarked_businesses, through: :bookmarks, class_name: 'Bookmark'
+
+  ### Authentication ###
+
+  # Token and digest creation for security.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def authenticated?(attribute,token)
+    digest = self.send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+
 end
