a85efc0e5dedf47a948e09f861e0ae3a3738bc4c
cjvirtucio87 <cjv287@gmail.com>
2016-08-22 21:33:55 -0700

    Verified working businesses#create

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -2,9 +2,20 @@ class BusinessesController < ApplicationController
   before_action :prep_soda
 
   def index
+    @businesses = Business.all
+  end
+
+  def create
     qparams = { '$q' => 'EASTMAN', '$limit' => '3' }
     data = query_soda(qparams)
-    raise
+    @business = Business.new(data.first.to_h)
+    if @business.save
+      flash[:success] = "Business saved."
+      redirect_to businesses_path
+    else
+      flash[:notice] = "Couldn't save business."
+      redirect_to businesses_path
+    end
   end
 
 end
